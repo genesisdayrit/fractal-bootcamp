3bcbe31d4de87d3498044bf9dc662c65d6832f61
genesisdayrit <genesisdayrit@gmail.com>
2025-09-30 16:42:36 -0400

    add migration for thread and thread messages

--- chatbot/app/db/schema.ts
+++ chatbot/app/db/schema.ts
@@ -1,1 +1,31 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, uuid, timestamp, text, json} from "drizzle-orm/pg-core";
+import { user } from './auth-schema'
+
+export const thread = pgTable('thread', {
+        id: uuid('id').primaryKey().defaultRandom(),
+        userId: text('user_id')
+            .notNull()
+            .references(() => user.id),
+        chatTitle: text('title'),
+        createdAt: timestamp('created_at').notNull().defaultNow(),
+        updatedAt: timestamp('updated_at')
+            .notNull()
+            .$onUpdate(() => new Date()),
+});
+
+export const threadMessage = pgTable('thread_message', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    userId: text('user_id')
+        .notNull()
+        .references(() => user.id),
+    threadId: uuid('thread_id')
+        .notNull()
+        .references(() => thread.id),
+    message: text('title'),
+    modelConfig: json('model_config'),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at')
+        .notNull()
+        .$onUpdate(() => new Date()),
+});
+

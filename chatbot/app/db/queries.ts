7c1554e761ccd0a7ab4f8585184327f8703d826c
genesisdayrit <genesisdayrit@gmail.com>
2025-10-01 14:52:59 -0400

    add insert chat and chat messages queries

---
+++ chatbot/app/db/queries.ts
@@ -0,0 +1,73 @@
+import { db } from './db'
+import { sql, eq } from 'drizzle-orm';
+import { chatTable, chatMessagesTable } from './schema';
+
+// test supabase connection
+export const testConnection = async () => {
+    try {
+
+        const rawStatement = sql`SELECT 1 FROM chat`
+        const result = await db.execute(rawStatement)
+        console.log('Supabase Successfully Connected')
+        console.log('DATABASE_URL exists:', !!process.env.DATABASE_URL)
+        return result
+    } catch (error) {
+        console.log('Error testing Supabase connection', error)
+    }
+}
+
+// return all chats for testing
+export const fetchChats = async () => {
+    try {
+        const rawStatement = sql`SELECT * FROM chat ORDER BY updated_at DESC`
+        const result = await db.execute(rawStatement)
+        
+        return result
+    } catch (error) {
+        console.error('Error fetching Game IDs:', error)
+        throw error
+    } finally {}
+}
+
+// start and insert a brand new chat thread
+export const createChat = async (id: string, userId: string) => {
+    try {
+        const chatId = id
+
+        const result = await db.insert(chatTable).values({
+            id: id,
+            userId: userId,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }).returning()
+
+        const newChatRecord = result[0]
+        console.log(newChatRecord)
+        return newChatRecord
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// insert a chat message that is already associated with a chat ID
+// figure out how to set flexible object type for modelConfig - since might not always have same config
+export const insertChatMessage = async (id: string, chatId: string, userId: string, message: string, role: string, modelConfig: any) => {
+    try {
+
+        const result = await db.insert(chatMessagesTable).values({
+            id: id,
+            chatId: chatId,
+            userId: userId,
+            message: message,
+            modelConfig: modelConfig,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }).returning()
+
+        const newChatMessageRecord = result[0]
+        console.log(newChatMessageRecord)
+        return newChatMessageRecord
+    } catch (error) {
+        console.log(error)
+    }
+}
--- chatbot/app/db/schema.ts
+++ chatbot/app/db/schema.ts
@@ -1,27 +1,28 @@
 import { integer, pgTable, varchar, uuid, timestamp, text, json} from "drizzle-orm/pg-core";
 import { user } from './auth-schema'
 
-export const chat = pgTable('chat', {
-        id: uuid('id').primaryKey().defaultRandom(),
-        userId: text('user_id')
-            .notNull()
-            .references(() => user.id),
-        chatTitle: text('title'),
-        createdAt: timestamp('created_at').notNull().defaultNow(),
-        updatedAt: timestamp('updated_at')
-            .notNull()
-            .$onUpdate(() => new Date()),
+export const chatTable = pgTable('chat', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    userId: text('user_id')
+        .notNull()
+        .references(() => user.id),
+    chatTitle: text('title'),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at')
+        .notNull()
+        .$onUpdate(() => new Date()),
 });
 
-export const chatMessage = pgTable('chat_message', {
+export const chatMessagesTable = pgTable('chat_message', {
     id: uuid('id').primaryKey().defaultRandom(),
     userId: text('user_id')
         .notNull()
         .references(() => user.id),
     chatId: uuid('chat_id')
         .notNull()
-        .references(() => chat.id),
+        .references(() => chatTable.id),
     message: text('title'),
+    role: text('role'),
     modelConfig: json('model_config'),
     createdAt: timestamp('created_at').notNull().defaultNow(),
     updatedAt: timestamp('updated_at')
@@ -29,3 +30,8 @@ export const chatMessage = pgTable('chat_message', {
         .$onUpdate(() => new Date()),
 });
 
+export type InsertChat = typeof chatTable.$inferInsert;
+export type SelectChat = typeof chatTable.$inferSelect;
+
+export type InsertChatMessage = typeof chatMessagesTable.$inferInsert;
+export type SelectChatMessage = typeof chatMessagesTable.$inferSelect;
